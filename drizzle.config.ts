a40282cb3881d5a173a2f61eae9418a36f78908b
wantossandro <wantossandro@gmail.com>
2026-04-30 21:46:22 +0700

    update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,18 +2,10 @@ import { defineConfig } from 'drizzle-kit';
 
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-// export default defineConfig({
-// 	schema: './src/lib/server/db/schema.ts',
-// 	dialect: 'postgresql',
-// 	dbCredentials: { url: process.env.DATABASE_URL },
-// 	verbose: true,
-// 	strict: true
-// });
-
-export default {
+export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
 	dialect: 'postgresql',
-	dbCredentials: {
-		url: process.env.DATABASE_URL,
-	},
-};
+	dbCredentials: { url: process.env.DATABASE_URL },
+	verbose: true,
+	strict: true
+});
